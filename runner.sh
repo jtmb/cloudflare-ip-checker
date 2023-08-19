ffabc33749c3a54a8e32bf3cb3570c91963fc971
jtmb <james.branco@gmail.com>
2023-08-19 08:27:01 -0400

    updates

--- runner.sh
+++ runner.sh
@@ -1,6 +1,7 @@
 export COLUMNS=80
 date=$(date)
+public_ip=$(curl icanhazip.com)
 
 # Uses Ansible and outputs logs with date of run
-ANSIBLE_CONFIG=./ansible.cfg \
- sudo ansible-playbook -i inventory.ini main.yml --vault-pass vault.yml  >./logs/"$date".log
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory.ini main.yml  --extra-vars "ssh_port=$SSH_PORT pub_key=$pub_key \
+ ansible_sudo_pass=RunStr8@1! PUB_IP=$public_ip" 
